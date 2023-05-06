6ce74d90f9393560d65d4294d79685828a0f0a74
nvware <h_valizadgan@yahoo.com>
2023-05-06 14:42:13 +0300

    Update RedirectedOutputTest.java

--- PreparationKits/OneMonthPreparationKit/src/test/java/org/nvware/RedirectedOutputTest.java
+++ PreparationKits/OneMonthPreparationKit/src/test/java/org/nvware/RedirectedOutputTest.java
@@ -21,27 +21,6 @@ public interface RedirectedOutputTest {
     public default String getOutput() {
         return outContent.toString();
     }
-//
-//    private  void testMainOutput(Runnable mainMethod, String expectedOutput) {
-//        setUpStreams(); // Redirect output to a ByteArrayOutputStream
-//
-//        // Call main method
-//        mainMethod.run();
-//
-//        // Verify the output
-//        String result = "";
-//        String outs = getOutput();
-//        try {
-//            assertEquals(expectedOutput, outs);
-//            result = "Test passed!";// Print out a message indicating that the test has passed
-//        } catch (AssertionError e) {
-//            result = "Wrong Answer :(";
-//        } finally {
-//            restoreStreams();// Restore the original System.out
-//            System.out.println("testMainOutput");
-//            System.out.println(result);
-//        }
-//    }
 
     public default void runTestCase(String input, String expectedOutput, Runnable mainMethod) {
         System.setIn(new ByteArrayInputStream(input.getBytes())); //set inputStream to system.in
